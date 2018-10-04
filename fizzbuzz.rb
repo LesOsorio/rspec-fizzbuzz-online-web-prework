2797c1486587036ddd291634affd86fb2018ed15
Leslie Osorio <lesosorio17@gmail.com>
2018-10-04 20:00:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(num)
   if  num % 3 == 0 && num % 5 == 0
-    return "Fizz"
+    return "FizzBuzz"
 
 elsif num % 5 == 0
   return "Buzz"
 
 elsif num % 3 == 0 
-  return "FizzBuzz"
+  return "Fizz"
 
 else num % 4 == 0
   return nil
